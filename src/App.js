3a523962c8bebeb2853924f782d8653ec879c9b8
Johnny <jaani.matharu@gmail.com>
2022-01-05 11:07:30 -0500

    added app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar';
+import landingPage from './pages/landingPage';
+import clubPage from './pages/clubPage';
+import Footer from './components/footer';
+import { HashRouter as Router } from 'react-router-dom'
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Router>
+<Navbar></Navbar>
+<Route exact path='/' component={landingPage} />
+<div>
+      <Switch>
+        
+          <Route exact path='/landingPage' component={landingPage} />
+    
+          <Route exact path='/clubPage' component={clubPage} />
+      
+      </Switch>
+      </div>
+      </Router>
+      <Footer></Footer>      
     </div>
   );
 }
